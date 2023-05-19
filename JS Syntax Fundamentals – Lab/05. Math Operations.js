8d4ba1a9bc8c35fb951531f5efb0deda50c5cbf5
Elmir Mustafov <114015749+NoireBH@users.noreply.github.com>
2023-05-19 11:55:52 +0300

    Added JS syntax fundamentals lab

---
+++ JS Syntax Fundamentals – Lab/05. Math Operations.js
@@ -0,0 +1,30 @@
+function solve(num1,num2,string){
+switch (string) {
+    case '+':
+        console.log(num1 + num2);
+        break;
+        case '+':
+        console.log(num1 + num2);
+        break;
+        case '-':
+        console.log(num1 - num2);
+        break;
+        case '*':
+        console.log(num1 * num2);
+        break;
+        case '/':
+        console.log(num1 / num2);
+        break;
+        case '**':
+        console.log(num1 ** num2);
+        break;
+        case '%':
+        console.log(num1 % num2);
+        break;
+
+    default:
+        break;
+}
+}
+
+solve(5,5,'*');
