949d8b76480a0fc538b44a7f8178f92308113cf5
Elmir Mustafov <114015749+NoireBH@users.noreply.github.com>
2023-06-30 13:54:48 +0300

    Added 05,06 dom ex

--- JS-Front-End-DOM-and-Events-Lab-Resources/05. Delete-From-Table/app.js
+++ JS-Front-End-DOM-and-Events-Lab-Resources/05. Delete-From-Table/app.js
@@ -1,3 +1,29 @@
 function deleteByEmail() {
-    console.log('TODO:...');
+    
+    let emailToDelete = document.getElementsByName('email')[0].value;
+
+    let emails = document.querySelectorAll('tr td:nth-child(2)');
+
+    let found = false;
+
+    for (const email of emails) {
+        
+        if (email.textContent === emailToDelete) {
+            
+            found = true;
+            let emailParent = email.parentNode;
+            emailParent.parentNode.removeChild(emailParent);
+            document.getElementById('result').textContent = 'Deleted';
+        }
+        
+    }
+
+    if (!found) {
+        
+        document.getElementById('result').textContent = 'Not found.';
+    }
+
+    else {
+        found = false;
+    }
 }
--- JS-Front-End-DOM-and-Events-Lab-Resources/06. Add-Delete/app.js
+++ JS-Front-End-DOM-and-Events-Lab-Resources/06. Add-Delete/app.js
@@ -1,3 +1,23 @@
 function addItem() {
-    //TODO...
+
+    let text = document.getElementById('newItemText').value;
+    let li = document.createElement('li');
+
+    li.appendChild(document.createTextNode(text));
+
+    let newAnchor = document.createElement('a');
+    newAnchor.textContent = '[Delete]';
+    newAnchor.addEventListener('click',deleteEmail);
+    li.appendChild(newAnchor);
+
+    document.getElementById('items').appendChild(li);
+
+    document.getElementById('newItemText').value = '';
+   
+}
+
+function deleteEmail(e){
+
+    let emailToDelete = e.currentTarget.parentElement;
+    emailToDelete.remove();
 }
