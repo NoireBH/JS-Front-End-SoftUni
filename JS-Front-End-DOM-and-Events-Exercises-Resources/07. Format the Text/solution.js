93f8b33bbb79c9e500e2d9e84030da5d2f33c596
Elmir Mustafov <114015749+NoireBH@users.noreply.github.com>
2023-07-03 11:47:19 +0300

    Added DOM ex

--- JS-Front-End-DOM-and-Events-Exercises-Resources/07. Format the Text/solution.js
+++ JS-Front-End-DOM-and-Events-Exercises-Resources/07. Format the Text/solution.js
@@ -1,3 +1,21 @@
 function solve() {
-  //TODO
+  
+    let textArea = document.getElementsByTagName('textarea')[0];
+    let output = document.getElementById('output');
+
+    let textAreaText = textArea.value;
+    let sentences = textAreaText.split('.');
+    sentences.pop();
+
+    
+    
+    while (sentences.length > 0) {
+      
+      let paragraphSentence = sentences.splice(0,3)
+      .map((sentence) => sentence.trimStart());
+
+      let p = document.createElement('p');
+      p.textContent = paragraphSentence.join('.') + '.';
+      output.appendChild(p);
+    }
 }
