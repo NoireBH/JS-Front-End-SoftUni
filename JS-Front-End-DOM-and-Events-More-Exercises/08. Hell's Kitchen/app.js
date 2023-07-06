0a1a165810c06058b5258deb0d1ea482a47990f6
Elmir Mustafov <114015749+NoireBH@users.noreply.github.com>
2023-07-06 10:41:14 +0300

    Added HTTP and Rest lab

--- JS-Front-End-DOM-and-Events-More-Exercises/08. Hell's Kitchen/app.js
+++ JS-Front-End-DOM-and-Events-More-Exercises/08. Hell's Kitchen/app.js
@@ -2,7 +2,49 @@ function solve() {
    document.querySelector('#btnSend').addEventListener('click', onClick);
 
    function onClick () {
-      //   TODO:
+      
+      let restaurantInfo = JSON.parse(document.querySelector('#inputs textarea').value);
+      let bestRestaurantOutput = document.querySelector('#bestRestaurant p');
+      let workersOutputElement = document.querySelector('#workers p');
+      let restaurantsArray = [];
+      
+      restaurantInfo.forEach(res => {
+         let restaurantInfo = res.split(' - ');
+         let restaurantName = restaurantInfo.shift();
+         let employees = restaurantInfo.pop().split(', ');
+         let employeeArray = [];
+         
+         for (const employee of employees) {
+            let [employeeName,salary] = employee.split(' ');
+            let salaryNum = Number(salary);
+            let employeeObj = {employeeName,salary: salaryNum};
+            employeeArray.push(employeeObj);
+         }
+
+         
+         let resObject = {restaurantName, employees : employeeArray};
+         restaurantsArray.push(resObject);
+
+      });
+
+      let bestRestaurant = 0;
+      let bestRestaurantaverageSalary = 0;
+      let bestRestaurantSalary = 0;
+
+      for (const restaurant of restaurantsArray) {
+         
+         let currSalaryTotal = 0;
+         restaurant.employees.forEach(e => {
+            currSalaryTotal +=e.salary;
+         });
+
+         let avg = currSalaryTotal / this.employees.length;
+         if (avg > bestRestaurantaverageSalary) {
+            bestRestaurantaverageSalary = avg;
+            bestRestaurant = restaurant.restaurantName;
+            bestRestaurantSalary;
+         }
+      }
       
    }
 }
