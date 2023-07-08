282523eac5b19cafbdb16c717296c3a89a84f6a6
Elmir Mustafov <114015749+NoireBH@users.noreply.github.com>
2023-07-08 14:01:06 +0300

    Added http and rest lab

--- JS-Front-End-HTTP-and-REST-Lab-Resources/04. GitHub-Repos/scripts/app.js
+++ JS-Front-End-HTTP-and-REST-Lab-Resources/04. GitHub-Repos/scripts/app.js
@@ -1,3 +1,10 @@
 function loadRepos() {
-   console.log("TODO...");
+
+   let result = document.getElementById('res');
+
+   fetch('https://api.github.com/users/testnakov/repos')
+   .then((data) => data.text())
+   .then((data) => {
+      result.textContent = data;
+   })
 }
--- JS-Front-End-HTTP-and-REST-Lab-Resources/05. Github-Repos-by-Username/scripts/app.js
+++ JS-Front-End-HTTP-and-REST-Lab-Resources/05. Github-Repos-by-Username/scripts/app.js
@@ -1,3 +1,21 @@
 function loadRepos() {
-	console.log("TODO...");
+	const BASE_URL = "https://api.github.com/users/";
+	let userName = document.getElementById('username').value;
+	 document.getElementById('repos').innerHTML = '';
+
+	fetch(BASE_URL + userName + "/repos")
+	.then((data) => data.json())
+	.then((repos) => {
+		for (const repo of repos) {
+			const list = document.getElementById('repos');
+			let li = document.createElement('li');
+			let a = document.createElement('a');
+			a.href = repo.html_url;
+			a.textContent = repo.full_name;
+			li.appendChild(a);
+			list.appendChild(li);
+		}
+		
+	});
+	
 }
