6bc754f64e362a783c0fb8e5c14ad11d28dd78b1
Elmir Mustafov <114015749+NoireBH@users.noreply.github.com>
2023-07-27 11:19:22 +0300

    added exam prep project

---
+++ Exam prep/01.HorseRacing/01.horseRacing.js
@@ -0,0 +1,78 @@
+function solve(input) {
+  let horseArray = input.shift().split("|");
+
+  let commandInfo = input.shift().split(" ");
+  let command = commandInfo[0];
+
+  while (command !== "Finish") {
+    if (command === "Retake") {
+      let overtakingHorseIndex = horseArray.indexOf(commandInfo[1]);
+      let overtakenHorseIndex = horseArray.indexOf(commandInfo[2]);
+
+      if (overtakingHorseIndex < overtakenHorseIndex) {
+        let overtakingHorse = horseArray[overtakingHorseIndex];
+        let overtakenHorse = horseArray[overtakenHorseIndex];
+
+        horseArray[overtakingHorseIndex] = overtakenHorse;
+        horseArray[overtakenHorseIndex] = overtakingHorse;
+        console.log(`${overtakingHorse} retakes ${overtakenHorse}.`);
+      }
+    } else if (command === "Trouble") {
+      let horse = commandInfo[1];
+      let horseIndex = horseArray.indexOf(horse);
+      if (horseIndex > 0) {
+        [horseArray[horseIndex], horseArray[horseIndex - 1]] = [
+          horseArray[horseIndex - 1],
+          horseArray[horseIndex],
+        ];
+        console.log(`Trouble for ${horse} - drops one position.`);
+      }
+    } else if (command === "Rage") {
+      let horse = commandInfo[1];
+      let horseIndex = horseArray.indexOf(horse);
+
+      if (horseIndex === horseArray.length - 2) {
+        let overtakingHorse = horseArray.splice(horseIndex, 1);
+        horseArray.push(overtakingHorse[0]);
+        console.log(`${horse} rages 2 positions ahead.`);
+      } else if (horseIndex < horseArray.length - 2) {
+        let overtakingHorse = horseArray.splice(horseIndex, 1);
+        let overtakenHorse = horseArray.splice(horseIndex + 1, 1);
+        horseArray.push(overtakenHorse[0]);
+        horseArray.push(overtakingHorse[0]);
+
+        console.log(`${overtakingHorse} rages 2 positions ahead.`);
+      }
+    } else {
+      let firstPlace = horseArray.pop();
+      let lastPlace = horseArray.shift();
+      horseArray.push(firstPlace);
+      horseArray.push(lastPlace);
+
+      console.log(`What a miracle - ${lastPlace} becomes first.`);
+    }
+
+    commandInfo = input.shift().split(" ");
+    command = commandInfo[0];
+  }
+
+  console.log(horseArray.join("->"));
+  console.log(`The winner is: ${horseArray[horseArray.length - 1]}`);
+}
+
+solve([
+  "Bella|Alexia|Sugar",
+  "Retake Alexia Sugar",
+  "Rage Bella",
+  "Trouble Bella",
+  "Finish",
+]);
+
+solve([
+  "Onyx|Domino|Sugar|Fiona",
+  "Trouble Onyx",
+  "Retake Onyx Sugar",
+  "Rage Domino",
+  "Miracle",
+  "Finish",
+]);
